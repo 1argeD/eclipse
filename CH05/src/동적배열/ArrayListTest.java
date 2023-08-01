422e3e2d454b37aa910f2ec9b54a9da4fb0a4fdb
HJH <gkswhdgur1234@naver.com>
2023-08-01 14:42:27 +0900

    <chore> 오타 수정

--- CH05/src/동적배열/ArrayListTest.java
+++ CH05/src/동적배열/ArrayListTest.java
@@ -13,21 +13,21 @@ public class ArrayListTest {
 	public static void main(String[] args) {
 		//arrayList
 		
-		list.add("첫번쨰 문장");//값추가
+		list.add("첫번째 문장");//값추가
 		list.add("두번째 문자");
 		list.add("세번째 문자");
 		System.out.println(list.get(1));//index 값을 통해서 접근할 수 있다.
 		list.remove(1);
-		System.out.println(list.get(1));//두번쨰 문자가 remove에 삭제되어 세번쨰 문자가 출력된다.
+		System.out.println(list.get(1));//두번째 문자가 remove에 삭제되어 세번째 문자가 출력된다.
 		
 		//linkedList
 		
-		list2.add("첫번쨰 문장");//값추가
+		list2.add("첫번째 문장");//값추가
 		list2.add("두번째 문자");
 		list2.add("세번째 문자");
 		System.out.println(list.get(1));//index 값을 통해서 접근할 수 있다.
 		list2.remove(1);
-		System.out.println(list.get(1));//두번쨰 문자가 remove에 삭제되어 세번쨰 문자가 출력된다.
+		System.out.println(list.get(1));//두번째 문자가 remove에 삭제되어 세번째 문자가 출력된다.
 		
 		
 		
