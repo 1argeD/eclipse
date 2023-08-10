d41f1f1c7657ad2efa9487ee3a5878b5e921303f
HJH <gkswhdgur1234@naver.com>
2023-08-10 12:45:25 +0900

    <feat>람다식문법

--- CH10/src/람다식문법/Lambda2Demo.java
+++ CH10/src/람다식문법/Lambda2Demo.java
@@ -4,6 +4,10 @@ interface N {
 	int neg(int x);
 }
 
+interface P {
+	void p();
+}
+
 public class Lambda2Demo {
 	public static void main(String[] args) {
 		N n;
@@ -14,7 +18,13 @@ public class Lambda2Demo {
 		n = x -> {return -x;};	//매게변수가 하나일 경우 관호 생략가능
 		n = x -> -x;	    	//실행문이 하나라면 중괄호와 세미콜론 생략가능(단, 실행문이 하나의 return문이라면 )
 								//return 키워드도 생략가능
-		
+	
+		/*void print() {
+			System.out.println("안녕");
+		}*/
+		P p;
+		 p = () -> {System.out.println("안녕");};
+		 p = () -> System.out.println("안녕");
 	}
 	
 	
